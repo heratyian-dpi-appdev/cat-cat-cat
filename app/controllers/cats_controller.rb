57316fdbdc0514bf7526dc9832e0cec3d3fd3874
Ian <heratyian@gmail.com>
2023-05-11 18:07:19 +0000

    Add bootstrap and cat resource

---
+++ app/controllers/cats_controller.rb
@@ -0,0 +1,54 @@
+class CatsController < ApplicationController
+  def index
+    matching_cats = Cat.all
+
+    @list_of_cats = matching_cats.order({ :created_at => :desc })
+
+    render({ :template => "cats/index.html.erb" })
+  end
+
+  def show
+    the_id = params.fetch("path_id")
+
+    matching_cats = Cat.where({ :id => the_id })
+
+    @the_cat = matching_cats.at(0)
+
+    render({ :template => "cats/show.html.erb" })
+  end
+
+  def create
+    the_cat = Cat.new
+    the_cat.name = params.fetch("query_name")
+
+    if the_cat.valid?
+      the_cat.save
+      redirect_to("/cats", { :notice => "Cat created successfully." })
+    else
+      redirect_to("/cats", { :alert => the_cat.errors.full_messages.to_sentence })
+    end
+  end
+
+  def update
+    the_id = params.fetch("path_id")
+    the_cat = Cat.where({ :id => the_id }).at(0)
+
+    the_cat.name = params.fetch("query_name")
+
+    if the_cat.valid?
+      the_cat.save
+      redirect_to("/cats/#{the_cat.id}", { :notice => "Cat updated successfully."} )
+    else
+      redirect_to("/cats/#{the_cat.id}", { :alert => the_cat.errors.full_messages.to_sentence })
+    end
+  end
+
+  def destroy
+    the_id = params.fetch("path_id")
+    the_cat = Cat.where({ :id => the_id }).at(0)
+
+    the_cat.destroy
+
+    redirect_to("/cats", { :notice => "Cat deleted successfully."} )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
+  root to: "cats#index"
 
+  # Routes for the Cat resource:
 
+  # CREATE
+  post("/insert_cat", { :controller => "cats", :action => "create" })
+
+  # READ
+  get("/cats", { :controller => "cats", :action => "index" })
+
+  get("/cats/:path_id", { :controller => "cats", :action => "show" })
+
+  # UPDATE
+
+  post("/modify_cat/:path_id", { :controller => "cats", :action => "update" })
+
+  # DELETE
+  get("/delete_cat/:path_id", { :controller => "cats", :action => "destroy" })
+
+  #------------------------------
 
 end
